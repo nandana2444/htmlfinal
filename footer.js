3c41d540f1a26e3f8604e722d2dc4b82a5c61704
nandana2444 <153043860+nandana2444@users.noreply.github.com>
2023-12-06 23:57:14 -0800

    Add files via upload

---
+++ footer.js
@@ -0,0 +1,7 @@
+// footer.js
+document.addEventListener("DOMContentLoaded", function () {
+    const footer = document.createElement("footer");
+    footer.innerHTML = "<p>&copy; " + new Date().getFullYear() + " PORSCHE 390. All rights reserved.</p>";
+    document.body.appendChild(footer);
+  });
+  
